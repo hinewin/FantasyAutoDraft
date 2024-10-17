75464ffec8d34b72c660b7f36417adc1a0234c95
(Rebase PR Action) <github_action@users.noreply.github.com>
2024-10-17 02:09:54 -0700

    draft engine complete

--- draft.py
+++ draft.py
@@ -31,18 +31,31 @@ class Draft:
                 Rank REAL PRIMARY KEY
                 );
         """)
-
+        print ("[DB] Created DraftedPlayers Table")
         # Create FantasyTeamStats table
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS FantasyTeamStats (
                 FantasyTeam TEXT PRIMARY KEY,
-                TotalPlayers INTEGER,
-                TotalPoints REAL,
-                PickOrderNum INTEGER
+                TotalGP REAL,
+                TotalMPG REAL,
+                TotalFG_Made REAL,
+                TotalFG_Attempt REAL,
+                TotalFT_Attempt REAL,
+                TotalFT_Made REAL,
+                TotalThree_Made REAL,
+                TotalPoints REAL, 
+                TotalRebounds REAL,
+                TotalAssists REAL,
+                TotalSteals REAL,
+                TotalBlocks REAL,
+                TotalTurnovers REAL,
+                TotalFantasyPt REAL
             );
-        """)
+         """)
 
         self.connection.commit()
+        print ("[DB] Created FantasyTeamStats Table")
+
 
     def reset_draft(self):
         """Reset the draft state - call this when we want to start over"""
@@ -117,11 +130,11 @@ class Draft:
 
     def draft_player(self):
         """ Draft player, writing result to CSV & Database"""
-
+        overall_pick = 1
         for row in self.draft_order:
             pick, team, _, _ = row
             while True:
-                print(f"Now drafting: {team} at pick #: {pick}")
+                print(f"Now drafting: {team} at pick #: {overall_pick}")
                 player_choice = input("Provide Player Name or Rank to Draft: ")
                 players = self.select_player(player_choice)
 
@@ -152,20 +165,22 @@ class Draft:
                 # Confirm with the user before drafting
                 confirm = input(f"Draft player {player['player']}, Rank {player['rank']}, from team {player['team']}, position {player['pos']}? (yes/no): ")
                 if confirm.lower() in ["y", "yes"]:
+                    row[0] = overall_pick
                     # Insert player data into your 'DraftedPlayers' table in the database
                     self.cursor.execute("""
                         INSERT INTO DraftedPlayers (FantasyTeam, DraftPick, Player, Rank)
                         VALUES (%s, %s, %s, %s)
-                    """, (team, pick, player['player'], player['rank']))  # team and pick come from the draft_order list
+                    """, (team, overall_pick, player['player'], player['rank']))  # team and pick come from the draft_order list
                     self.connection.commit()
+                    print ("[DB] Inserted values into DraftedPlayers Table")
                     # Save the drafted player's name and rank into the draft order list
                     row[2] = player['player']
                     row[3] = player['rank']
                     print(f"Player {player['player']} is successfully drafted by {team}!")
+                    overall_pick += 1
                     break
                 else:
                     print("Draft cancelled for this player. Please choose another player.")
-                
 
         # At the end of the drafting, save the draft order with all players into a CSV file
         with open('draft_order.csv', 'w', newline='', encoding='utf-8') as csvfile:
@@ -176,11 +191,125 @@ class Draft:
         print("Drafting complete. The final draft order has been saved to draft_order.csv")
                 
     def calculate_team_stats(self, team):
-        pass
+        """Calculates and updates statistics for a particular fantasy team."""
+       
+        # Fetch all players from drafted players for the team
+        self.cursor.execute("""
+            SELECT Rank FROM DraftedPlayers WHERE FantasyTeam = %s
+        """, (team,))
 
+        drafted_ranks = self.cursor.fetchall()
 
+        # Initialize stats
+        total_stats = {
+        "TotalGP": 0,
+        "TotalMPG": 0,
+        "TotalFG_Made": 0,
+        "TotalFG_Attempt": 0,
+        "TotalFT_Attempt": 0,
+        "TotalFT_Made": 0,
+        "TotalThree_Made": 0,
+        "TotalPoints": 0, 
+        "TotalRebounds": 0,
+        "TotalAssists": 0,
+        "TotalSteals": 0,
+        "TotalBlocks": 0,
+        "TotalTurnovers": 0,
+        "TotalFantasyPt": 0
+        }
 
-    
+        # Iterate over each player and get their stats
+        for (rank,) in drafted_ranks:
+            self.cursor.execute("""
+                SELECT GP, MPG, FG_Made, FG_Attempted, FT_Attempted, FT_Made, ThreePM, 
+                Pts, TREB, AST, STL, BLK, TOs, TOTAL 
+                FROM PlayerStats WHERE Rank = %s
+            """, (rank,))
+
+            player_stats = self.cursor.fetchone()
+            # Update stats
+            total_stats["TotalGP"] += player_stats["gp"]
+            total_stats["TotalMPG"] += player_stats["mpg"]
+            total_stats["TotalFG_Made"] += player_stats["fg_made"]
+            total_stats["TotalFG_Attempt"] += player_stats["fg_attempted"]
+            total_stats["TotalFT_Attempt"] += player_stats["ft_attempted"]
+            total_stats["TotalFT_Made"] += player_stats["ft_made"]
+            total_stats["TotalThree_Made"] += player_stats["threepm"]
+            total_stats["TotalPoints"] += player_stats["pts"]
+            total_stats["TotalRebounds"] += player_stats["treb"]
+            total_stats["TotalAssists"] += player_stats["ast"]
+            total_stats["TotalSteals"] += player_stats["stl"]
+            total_stats["TotalBlocks"] += player_stats["blk"]
+            total_stats["TotalTurnovers"] += player_stats["tos"]
+            total_stats["TotalFantasyPt"] += player_stats["total"]
+
+        # Insert to `FantasyTeamStats` Table
+        self.cursor.execute("""
+                INSERT INTO FantasyTeamStats (
+                    FantasyTeam, 
+                    TotalGP, 
+                    TotalMPG, 
+                    TotalFG_Made, 
+                    TotalFG_Attempt, 
+                    TotalFT_Attempt, 
+                    TotalFT_Made, 
+                    TotalThree_Made, 
+                    TotalPoints, 
+                    TotalRebounds, 
+                    TotalAssists, 
+                    TotalSteals, 
+                    TotalBlocks, 
+                    TotalTurnovers, 
+                    TotalFantasyPt)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (FantasyTeam) DO UPDATE SET 
+                TotalGP=excluded.TotalGP, 
+                TotalMPG=excluded.TotalMPG, 
+                TotalFG_Made=excluded.TotalFG_Made,
+                TotalFG_Attempt=excluded.TotalFG_Attempt,
+                TotalFT_Attempt=excluded.TotalFT_Attempt,
+                TotalFT_Made=excluded.TotalFT_Made,
+                TotalThree_Made=excluded.TotalThree_Made,
+                TotalPoints=excluded.TotalPoints,
+                TotalRebounds=excluded.TotalRebounds,
+                TotalAssists=excluded.TotalAssists,
+                TotalSteals=excluded.TotalSteals,
+                TotalBlocks=excluded.TotalBlocks,
+                TotalTurnovers=excluded.TotalTurnovers,
+                TotalFantasyPt=excluded.TotalFantasyPt
+    """, (team,
+          total_stats["TotalGP"], 
+          total_stats["TotalMPG"], 
+          total_stats["TotalFG_Made"], 
+          total_stats["TotalFG_Attempt"], 
+          total_stats["TotalFT_Attempt"], 
+          total_stats["TotalFT_Made"], 
+          total_stats["TotalThree_Made"], 
+          total_stats["TotalPoints"], 
+          total_stats["TotalRebounds"], 
+          total_stats["TotalAssists"], 
+          total_stats["TotalSteals"], 
+          total_stats["TotalBlocks"], 
+          total_stats["TotalTurnovers"], 
+          total_stats["TotalFantasyPt"]))
+        self.connection.commit()
+
+    def show_team_stats(self):
+        """Retrieves and shows the total statistics for each fantasy team."""
+
+        # Retrieve the stats for all teams
+        self.cursor.execute("""
+            SELECT * FROM FantasyTeamStats
+        """)
+
+        stats = self.cursor.fetchall()
+
+        # Now print the stats for each team
+        for stat in stats:
+            print(f"Stats for {stat['fantasyteam']}:")
+            print(f"Total GP: {stat['totalgp']}")
+            print(f"Total MPG: {stat['totalmpg']}")
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-R", "--reset-draft", help="Reset draft", action="store_true")
@@ -203,10 +332,12 @@ def main():
         
         if args.start_draft:
             draft.start_draft()
-            draft.create_draft_order()
+            draft.create_draft_order()                
             draft.draft_player()
 
-        
+            for _, team, _, _ in draft.draft_order:
+                draft.calculate_team_stats(team)
+            draft.show_team_stats()
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
